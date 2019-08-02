05197372fd6e671544fbe87d84c9b01c0fc9a7f4
nguyenhaminhtuan <nguyenhaminhtuan1997@gmail.com>
2019-08-02 20:51:42 +0700

    add bcrypt methods and statics for model

--- src/models/User.js
+++ src/models/User.js
@@ -1,13 +1,41 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const config = require('../config');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   address: { type: String },
-  orders: { type: Schema.Types.ObjectId, ref: 'Order' },
+  orders: [{ type: Schema.Types.ObjectId, ref: 'Order' }],
   createAt: { type: Date, default: Date.now() }
 });
+
+userSchema.statics.generateHash = function(plainText) {
+  return new Promise(function(resolve, resject) {
+    bcrypt.hash(plainText, config.bcrypt.saltRound, function(err, encrypted) {
+      if (err) {
+        resject(err);
+      } else {
+        resolve(encrypted);
+      }
+    });
+  });
+};
+
+userSchema.methods.comparePassword = function(password) {
+  const encrypted = this.password;
+
+  return new Promise(function(resolve, reject) {
+    bcrypt.compare(password, encrypted, function(err, isMatch) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(isMatch);
+      }
+    });
+  });
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
