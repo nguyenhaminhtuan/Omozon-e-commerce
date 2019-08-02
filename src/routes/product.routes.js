7ea2b1984987a54e77e7924ae8974e43e06ee714
nguyenhaminhtuan <nguyenhaminhtuan1997@gmail.com>
2019-08-03 01:19:55 +0700

    routes for all and admin

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
+const authCheck = require('../middlewares/auth');
 const productController = require('../controllers/product.controller');
 
-router.get('/', productController.testController);
+// Routes for all
+router.get('/', productController.fetchProdcut);
+router.get('/:_id', productController.getProductById);
 
+// Admin routes
+router.post('/add', authCheck, productController.addProduct);
+router.put('/update/:_id', authCheck, productController.updateProduct);
+router.delete('/remove/:_id', authCheck, productController.removeProduct);
 module.exports = router;
