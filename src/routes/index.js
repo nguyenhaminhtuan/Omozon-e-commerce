3386b594579c4eaccebed369467df90f981ce257
nguyenhaminhtuan <nguyenhaminhtuan1997@gmail.com>
2019-08-02 21:29:24 +0700

    add auth route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./user.routes');
+const authRoutes = require('./auth.routes');
 const productRoutes = require('./product.routes');
 const categoryRoutes = require('./category.routes');
 
 router.use('/user', userRoutes);
+router.use('/auth', authRoutes);
 router.use('/product', productRoutes);
 router.use('/category', categoryRoutes);
 
