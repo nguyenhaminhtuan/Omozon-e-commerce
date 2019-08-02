46dfe858605cb64234eda2a50fa57057ca75eec8
nguyenhaminhtuan <nguyenhaminhtuan1997@gmail.com>
2019-08-02 21:30:15 +0700

    view profile and update profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,56 @@
+const User = require('../models/User');
+
 exports.testController = function(req, res) {
   res.status(200).json({ msg: 'OK' });
+};
+
+exports.viewProfile = async function(req, res, next) {
+  try {
+    const profile = await User.findById(req.user.id);
+
+    if (!profile) {
+      return res.status(400).json({ success: false, message: 'Invalid token' });
+    } else {
+      return res.status(200).json({
+        success: true,
+        message: 'Get profile user successfully',
+        profile
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateProfile = async function(req, res, next) {
+  try {
+    const { user } = req.body;
+    const profile = await User.findById(req.user.id);
+
+    if (!user) {
+      return res.status(403).json({ success: false, message: 'Invalid token' });
+    } else {
+      if (user.oldPassword) {
+        const isMatch = await profile.comparePassword(user.oldPassword);
+
+        if (isMatch) {
+          const hash = await User.generateHash(user.newPassword);
+          profile.password = hash;
+        } else {
+          return res
+            .status(400)
+            .json({ success: false, message: 'Password incorect' });
+        }
+      }
+
+      profile.address = user.address;
+      const profileUpdated = await profile.save();
+
+      return res
+        .status(200)
+        .json({ success: true, message: 'Updated profile', profileUpdated });
+    }
+  } catch (error) {
+    next(error);
+  }
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const userController = require('../controllers/user.controller');
+const authCheck = require('../middlewares/auth');
 
 router.get('/', userController.testController);
+router.get('/profile', authCheck, userController.viewProfile);
+router.post('/profile/update', authCheck, userController.updateProfile);
 
 module.exports = router;
