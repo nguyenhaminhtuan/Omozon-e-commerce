621536ee518f4c7db69f5883df07d45a19f1fadb
nguyenhaminhtuan <nguyenhaminhtuan1997@gmail.com>
2019-08-02 20:26:24 +0700

    config jwt and bcrypt

---
+++ src/config/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+module.exports = {
+  node_env: process.env.NODE_ENV,
+  app: {
+    port: process.env.APP_PORT
+  },
+  db: {
+    uri: process.env.DB_URI
+  },
+  bcrypt: {
+    saltRound: process.env.SALTROUND || 10
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expired: '45m'
+  }
+};
