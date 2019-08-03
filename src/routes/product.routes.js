cd6a30dbdfdbda7e5fc4aa4d09d3cd3feb35b6a1
nguyenhaminhtuan <nguyenhaminhtuan1997@gmail.com>
2019-08-03 20:14:23 +0700

    apply roles check middleware for admin routes

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const authCheck = require('../middlewares/auth');
+const rolesCheck = require('../middlewares/rolesCheck');
 const productController = require('../controllers/product.controller');
 
 // Routes for all
@@ -7,7 +8,17 @@ router.get('/', productController.fetchProdcut);
 router.get('/:_id', productController.getProductById);
 
 // Admin routes
-router.post('/add', authCheck, productController.addProduct);
-router.put('/update/:_id', authCheck, productController.updateProduct);
-router.delete('/remove/:_id', authCheck, productController.removeProduct);
+router.post('/add', authCheck, rolesCheck, productController.addProduct);
+router.put(
+  '/update/:_id',
+  authCheck,
+  rolesCheck,
+  productController.updateProduct
+);
+router.delete(
+  '/remove/:_id',
+  authCheck,
+  rolesCheck,
+  productController.removeProduct
+);
 module.exports = router;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const userController = require('../controllers/user.controller');
 const authCheck = require('../middlewares/auth');
+const rolesCheck = require('../middlewares/rolesCheck');
 
 // Check authentication for all user routes
 router.use(authCheck);
 
 // Admin routes
-router.get('/', userController.getAllUser);
-router.get('/', userController.getUserById);
+router.get('/', rolesCheck, userController.getAllUser);
+router.get('/', rolesCheck, userController.getUserById);
 
 // User routes
 router.get('/profile', userController.viewProfile);
