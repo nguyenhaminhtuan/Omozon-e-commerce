28fa0391a21dbc1743da712fc9748e807d614eef
nguyenhaminhtuan <nguyenhaminhtuan1997@gmail.com>
2019-08-03 21:28:24 +0700

    restful api for category

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,3 +1,82 @@
-exports.testController = function(req, res) {
-  res.status(200).json({ msg: 'OK' });
+const Category = require('../models/Category');
+
+exports.getAllCategory = async function(req, res, next) {
+  try {
+    const categories = await Category.find({});
+    return res
+      .status(200)
+      .json({ success: true, message: 'Get all Category success', categories });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getCategoryById = async function(req, res, next) {
+  try {
+    const { _id } = req.params;
+    const category = await Category.findById(_id);
+
+    if (category) {
+      return res
+        .status(200)
+        .json({ success: true, message: `Category ${category._id}` });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.addProduct = async function(req, res, next) {
+  try {
+    const { name } = req.body;
+    const isExisted = await Category.findOne({ name });
+
+    if (isExisted) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Category name already exist' });
+    } else {
+      const newCategory = new Category({
+        name
+      });
+      const category = await newCategory.save();
+
+      return res
+        .status(200)
+        .json({ success: true, message: 'Category added', category });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateCategory = async function(req, res, next) {
+  try {
+    const { _id } = req.params;
+    const { name } = req.body;
+    const category = await Category.updateOne({ _id }, { name });
+
+    if (category) {
+      return res
+        .status(200)
+        .json({ success: true, message: 'Category updated', category });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.removeProduct = async function(req, res, next) {
+  try {
+    const { _id } = req.params;
+    const category = await Category.deleteOne({ _id });
+
+    if (category) {
+      return res
+        .status(200)
+        .json({ success: true, message: 'Category deleted', category });
+    }
+  } catch (error) {
+    next(error);
+  }
 };
--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,6 +1,25 @@
 const router = require('express').Router();
+const authCheck = require('../middlewares/auth');
+const rolesCheck = require('../middlewares/rolesCheck');
 const categoryController = require('../controllers/category.controller');
 
-router.get('/', categoryController.testController);
+// Routes for all
+router.get('/', categoryController.getAllCategory);
+router.get('/:_id', categoryController.getCategoryById);
+
+// Routes for admin
+router.post('/add', categoryController.addProduct);
+router.put(
+  '/update/:_id',
+  authCheck,
+  rolesCheck,
+  categoryController.updateCategory
+);
+router.delete(
+  '/remove/:_id',
+  authCheck,
+  rolesCheck,
+  categoryController.removeProduct
+);
 
 module.exports = router;
