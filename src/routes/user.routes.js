a08b57b8b54b985303dc4ad2758367435c0f6782
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-06 20:27:40 +0700

    fix bugs joi string doesn't require args

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,13 +2,15 @@ const { Router } = require('express');
 const userController = require('../controllers/user.controller');
 const auth = require('../middlewares/auth');
 const admin = require('../middlewares/admin');
+const userSchema = require('../validations/user.validate');
+const validate = require('../middlewares/validate');
 
 const router = Router();
 
 // For all
 router.post('/', userController.createUser);
 // Admin routes
-router.get('/', auth, admin, userController.getAllUser);
+router.get('/', auth, admin, userController.fetchUser);
 router.get('/:id', auth, admin, userController.getUserById);
 // User routes
 router.get('/profile', auth, userController.viewProfile);
