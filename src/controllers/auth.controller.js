651670f462dace789abd162d7900287deca6f56a
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-06 22:04:10 +0700

    using try catch instead middleware

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,46 +2,53 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const config = require('../config');
 
-exports.login = async function(req, res) {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
+exports.login = async function(req, res, next) {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
-  if (!user) {
-    return res.status(400).json({ success: false, message: 'Incorrect email' });
-  }
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Incorrect email' });
+    }
 
-  const isMatch = await user.comparePassword(password);
+    const isMatch = await user.comparePassword(password);
 
-  if (!isMatch) {
-    return res
-      .status(400)
-      .json({ success: false, message: 'Incorrect password' });
-  } else {
-    const token = jwt.sign(
-      { id: user.id, isAdmin: user.isAdmin },
-      config.jwt.secret,
-      {
-        expiresIn: config.jwt.expired
-      }
-    );
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Incorrect password' });
+    } else {
+      const token = jwt.sign(
+        { id: user.id, isAdmin: user.isAdmin },
+        config.jwt.secret,
+        {
+          expiresIn: config.jwt.expired
+        }
+      );
 
-    return res
-      .status(200)
-      .json({ success: true, message: 'Login successfully', token });
+      return res
+        .status(200)
+        .json({ success: true, message: 'Login successfully', token });
+    }
+  } catch (error) {
+    next(error);
   }
 };
 
 // Only for development
 exports.createAdmin = async function(req, res) {
-  const username = 'admin';
+  const email = 'admin@admin';
   const password = 'admin';
   const hash = await User.generateHash(password);
   const admin = new User({
-    username,
+    email,
     password: hash,
+    name: 'admin',
     isAdmin: true
   });
   await admin.save();
 
-  res.status(200).json({ message: 'Admin created' });
+  return res.status(200).json({ message: 'Admin created' });
 };
