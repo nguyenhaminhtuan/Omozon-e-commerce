6c60ccfc6ba133ecd2e280a311c2eeb9538960cc
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-06 20:08:57 +0700

    fix bugs and add missing properties

--- src/validations/auth.validate.js
+++ src/validations/auth.validate.js
@@ -15,25 +15,5 @@ module.exports = {
         .required()
     })
     // with(key, peers), peers can be a one peer or array peers. For each of these peers being present, key also must be present for the schema to be valid.
-    .with('username', 'password'),
-  register: Joi.object().keys({
-    email: Joi.string()
-      .email()
-      .min(3)
-      .max(150)
-      .required(),
-    password: Joi.string()
-      .alphanum()
-      .min(4)
-      .max(150)
-      .required(),
-    name: Joi.string()
-      .min(3)
-      .max(255)
-      .required(),
-    adress: Joi.string()
-      .min(6)
-      .max(255)
-      .required()
-  })
+    .with('email', 'password')
 };
--- src/validations/user.validate.js
+++ src/validations/user.validate.js
@@ -1,6 +1,26 @@
 const Joi = require('joi');
 
 module.exports = {
+  create: Joi.object().keys({
+    email: Joi.string()
+      .email()
+      .min(3)
+      .max(150)
+      .required(),
+    password: Joi.string()
+      .alphanum()
+      .min(4)
+      .max(150)
+      .required(),
+    name: Joi.string()
+      .min(3)
+      .max(255)
+      .required(),
+    adress: Joi.string()
+      .min(6)
+      .max(255)
+      .required()
+  }),
   user: Joi.object().keys({
     name: Joi.string()
       .alphanum()
