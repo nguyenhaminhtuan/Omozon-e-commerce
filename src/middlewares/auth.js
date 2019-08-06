2585b1042f9a83e01dd7cb29eee42c6c05a5794e
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-06 22:10:29 +0700

    using x-auth-token instead bearer

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,22 +1,17 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config');
 
-module.exports = function(req, res, next) {
-  const header = req.headers['authorization']; // Get header
+module.exports = async function(req, res, next) {
+  const token = req.header('x-auth-token');
 
-  if (typeof header === 'undefined') {
-    return res.status(401).json({ success: false, msg: 'Unauthorization' });
-  } else {
-    if (header.startsWith('Bearer')) {
-      // Authorization: Bearer <access_token>
-      const token = header.split(' ')[1]; // Get token from index 1 in header string
-      // Contain check and throw error untrusted, expires,... If no error, return decoded
-      const decoded = jwt.verify(token, config.jwt.secret);
-      req.user = decoded;
+  if (!token)
+    return res.status(401).json({ success: false, message: 'Unauthorization' });
 
-      next();
-    } else {
-      return res.status(400).json({ success: false, msg: 'Invalid token' });
-    }
+  try {
+    const decoded = jwt.verify(token, config.jwt.secret);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(400).json({ success: false, message: 'Invalid token' });
   }
 };
