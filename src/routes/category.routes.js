f82c8b468c461d1dd0c950cb1947d3b0e18c98db
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-06 23:22:54 +0700

    add validate

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -2,20 +2,41 @@ const { Router } = require('express');
 const auth = require('../middlewares/auth');
 const admin = require('../middlewares/admin');
 const categoryController = require('../controllers/category.controller');
+const categorySchema = require('../validations/category.validate');
+const validate = require('../middlewares/validate');
 
 const router = Router();
 
-router.put('/:id/add-product', auth, admin, categoryController.addProduct);
+router.put(
+  '/:id/add-product',
+  auth,
+  admin,
+  validate(categorySchema.product),
+  categoryController.addProduct
+);
 router.put(
   '/:id/remove-product',
   auth,
   admin,
+  validate(categorySchema.product),
   categoryController.removeProduct
 );
 router.get('/', categoryController.fetchCategory);
 router.get('/:id', categoryController.getCategoryById);
-router.post('/', auth, admin, categoryController.addCategory);
-router.put('/:id', auth, admin, categoryController.updateCategory);
+router.post(
+  '/',
+  auth,
+  admin,
+  validate(categorySchema.category),
+  categoryController.addCategory
+);
+router.put(
+  '/:id',
+  auth,
+  admin,
+  validate(categorySchema.category),
+  categoryController.updateCategory
+);
 router.delete('/:id', auth, admin, categoryController.removeCategory);
 
 module.exports = router;
