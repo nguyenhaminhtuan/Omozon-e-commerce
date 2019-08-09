f754afc1e83bf5088a027b045f792ff7a62f88b2
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-09 22:04:13 +0700

    added products router

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const categoryRoutes = require('./category.routes');
+const productRotes = require('../routes/product.routes');
 
 const router = express.Router();
 
 router.use('/categories', categoryRoutes);
+router.use('/products', productRotes);
 
 module.exports = router;
