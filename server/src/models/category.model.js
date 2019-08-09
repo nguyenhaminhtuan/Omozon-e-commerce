3629c6188159bb56b40c850653ec7a45c0d408bf
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-09 22:11:12 +0700

    add unique and default value

--- server/src/models/category.model.js
+++ server/src/models/category.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   products: [{ type: mongoose.Types.ObjectId, ref: 'Product' }],
   createAt: { type: Date, default: Date.now() }
 });
--- server/src/models/product.model.js
+++ server/src/models/product.model.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   price: { type: Number, min: 10000, required: true },
   brand: { type: String, required: true },
-  description: { type: String, required: true },
+  description: { type: String, default: '' },
   category: { type: mongoose.Types.ObjectId, ref: 'Category', required: true },
   createAt: { type: Date, default: Date.now() }
 });
