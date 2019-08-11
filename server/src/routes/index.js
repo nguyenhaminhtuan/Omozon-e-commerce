d7812b37617b9398d9f49a3307115bdefef7de8b
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-12 00:11:26 +0700

    add authentication route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const categoryRoutes = require('./category.routes');
 const productRotes = require('../routes/product.routes');
+const authRoutes = require('./auth.routes');
 
 const router = express.Router();
 
 router.use('/categories', categoryRoutes);
 router.use('/products', productRotes);
+router.use('/auth', authRoutes);
 
 module.exports = router;
