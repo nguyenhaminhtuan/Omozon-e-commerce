d3d5952d5563c302cbb6a07fe29ab84b0ff0583d
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-12 00:10:47 +0700

    add middleware save, hash and method compare bcrypt

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -8,11 +9,31 @@ const userSchema = new mongoose.Schema({
     unique: true,
     required: true
   },
-  password: { type: String, minlength: 6, maxlength: 150, required: true },
+  password: {
+    type: String,
+    minlength: 6,
+    maxlength: 150,
+    select: false,
+    required: true
+  },
   name: { type: String, required: true },
   admin: { type: Boolean, default: false, required: true },
   creatAt: { type: Date, default: Date.now() }
 });
+
+userSchema.pre('save', async function(next) {
+  try {
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+  } catch (error) {
+    return new Error(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function(password) {
+  const match = await bcrypt.compare(password, this.password);
+  return match;
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
