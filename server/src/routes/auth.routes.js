58ce1f53cf3ddd7a4be0baa92f17f3a72676ad47
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-12 14:58:52 +0700

    add validate for login and signup route

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const authController = require('../controllers/auth.controller');
+const authValidate = require('../validation/auth.validate');
+const validate = require('../utils/validate');
 
 const router = express.Router();
 
-router.post('/login', authController.login);
-router.post('/signup', authController.signup);
+router.post('/login', validate(authValidate.login), authController.login);
+router.post('/signup', validate(authValidate.signup), authController.signup);
 
 module.exports = router;
