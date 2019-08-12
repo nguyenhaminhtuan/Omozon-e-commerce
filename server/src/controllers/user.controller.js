c5c18cbf28e1843fa896549e48606c7e6f7a6342
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-12 22:44:11 +0700

    add user profile

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -16,3 +16,32 @@ exports.getUser = catchAsync(async (req, res) => {
 
   return res.status(200).json({ message: 'Get user success', data: { user } });
 });
+
+exports.viewProfile = catchAsync(async (req, res) => {
+  const profile = await User.findById(req.user.id);
+
+  return res
+    .status(200)
+    .json({ message: 'Get profile success', data: { profile } });
+});
+
+// exports.updateProfile = catchAsync(async (req, res) => {
+//   const profile = await User.findByIdAndUpdate(req.user.id, { $set: req.body });
+
+//   return res
+//     .status(200)
+//     .json({ message: 'Profile updated', data: { profile } });
+// });
+
+exports.changePassword = catchAsync(async (req, res) => {
+  const { newPassword, oldPassword } = req.body;
+  const profile = await User.findById(req.user.id).select('+password');
+  const isMatch = await profile.comparePassword(oldPassword);
+
+  if (!isMatch) return res.status(400).json({ message: 'Incorrect password' });
+
+  profile.password = newPassword;
+  await profile.save();
+
+  return res.status(200).json({ message: 'Password changed' });
+});
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const auth = require('../utils/auth');
+const admin = require('../utils/admin');
 const userController = require('../controllers/user.controller');
 
 const router = express.Router();
 
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUser);
+router.get('/profile', auth, userController.viewProfile);
+// router.post('/profile/update', auth, userController.updateProfile);
+router.post('/profile/change-password', auth, userController.changePassword);
+
+router.get('/', auth, admin, userController.getAllUsers);
+router.get('/:id', auth, admin, userController.getUser);
 
 module.exports = router;
