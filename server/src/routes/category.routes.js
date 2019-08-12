9bd145a3db9cc4b7f819b5ecb72c246e4af71d91
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-12 22:57:37 +0700

    apply auth and admin check for protect route

--- server/src/routes/category.routes.js
+++ server/src/routes/category.routes.js
@@ -2,18 +2,30 @@ const express = require('express');
 const categoryController = require('../controllers/category.controller');
 const categoryValidate = require('../validation/category.validate');
 const validate = require('../utils/validate');
+const auth = require('../utils/auth');
+const admin = require('../utils/admin');
 
 const router = express.Router();
 
 router
   .route('/')
   .get(categoryController.getAllCategories)
-  .post(validate(categoryValidate.category), categoryController.createCategory);
+  .post(
+    auth,
+    admin,
+    validate(categoryValidate.category),
+    categoryController.createCategory
+  );
 
 router
   .route('/:id')
   .get(categoryController.getCategory)
-  .put(validate(categoryValidate.category), categoryController.updateCategory)
-  .delete(categoryController.deleteCategory);
+  .put(
+    auth,
+    admin,
+    validate(categoryValidate.category),
+    categoryController.updateCategory
+  )
+  .delete(auth, admin, categoryController.deleteCategory);
 
 module.exports = router;
--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -2,18 +2,30 @@ const express = require('express');
 const productController = require('../controllers/product.controller');
 const productValidate = require('../validation/product.validate');
 const validate = require('../utils/validate');
+const auth = require('../utils/auth');
+const admin = require('../utils/admin');
 
 const router = express.Router();
 
 router
   .route('/')
   .get(productController.getAllProducts)
-  .post(validate(productValidate.product), productController.createProduct);
+  .post(
+    auth,
+    admin,
+    validate(productValidate.product),
+    productController.createProduct
+  );
 
 router
   .route('/:id')
   .get(productController.getProduct)
-  .put(validate(productValidate.product), productController.updateProduct)
+  .put(
+    auth,
+    admin,
+    validate(productValidate.product),
+    productController.updateProduct
+  )
   .delete(productController.deleteProduct);
 
 module.exports = router;
