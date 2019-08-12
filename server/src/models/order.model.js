b3bf0674a7d434eace6e7ad5bdce015220249c29
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-12 23:06:03 +0700

    order model

---
+++ server/src/models/order.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  product: { type: mongoose.Types.ObjectId, ref: 'Product', required: true },
+  user: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
+  paid: { type: Boolean, default: false },
+  orderAt: { type: Date, default: Date.now() }
+});
+
+const Order = mongoose.model('Order', orderSchema);
+module.exports = Order;
