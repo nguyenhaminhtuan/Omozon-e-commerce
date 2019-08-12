fdb02ba9da9e58b5ffe18ac1760fc1bb6006c98a
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-12 15:08:08 +0700

    add user routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const categoryRoutes = require('./category.routes');
 const productRotes = require('../routes/product.routes');
 const authRoutes = require('./auth.routes');
+const userRoutes = require('./user.routes');
 
 const router = express.Router();
 
 router.use('/categories', categoryRoutes);
 router.use('/products', productRotes);
 router.use('/auth', authRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
