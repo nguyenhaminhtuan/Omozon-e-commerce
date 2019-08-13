d1fa92dc1b9a1613cedd2e4c7cea29b21d824809
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-13 22:21:59 +0700

    add image field

--- server/src/models/product.model.js
+++ server/src/models/product.model.js
@@ -4,6 +4,7 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   price: { type: Number, min: 10000, required: true },
   brand: { type: String, required: true },
+  image: { type: String, default: 'default.png' },
   description: { type: String, default: '' },
   category: { type: mongoose.Types.ObjectId, ref: 'Category', required: true },
   createAt: { type: Date, default: Date.now() }
