c4125b04dbdae37bd01a057eb28dff0da007303b
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-13 13:02:48 +0700

    cart route for user

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -4,6 +4,8 @@ const admin = require('../utils/admin');
 const userController = require('../controllers/user.controller');
 const userValidate = require('../validation/user.validate');
 const validate = require('../utils/validate');
+const cartController = require('../controllers/cart.controller');
+const orderController = require('../controllers/order.controller');
 
 const router = express.Router();
 
@@ -15,6 +17,21 @@ router.post(
   validate(userValidate.changePassword),
   userController.changePassword
 );
+
+router.get('/orders', auth, orderController.getOrdersHistory);
+
+router
+  .route('/carts')
+  .get(auth, cartController.getAllCarts)
+  .post(auth, cartController.createCart);
+
+router.patch('/carts/quantity/:id', auth, cartController.updateQuantity);
+
+router
+  .route('/carts/:id')
+  .get(auth, cartController.getCart)
+  .patch(auth, cartController.paid)
+  .delete(auth, cartController.removeCart);
 
 router.get('/', auth, admin, userController.getAllUsers);
 router.get('/:id', auth, admin, userController.getUser);
