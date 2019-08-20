911a045f82cf2b506a9de077b260d871ddee9cf4
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-20 23:29:15 +0700

    add and setup react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import AdminNavbar from './Components/Layouts/AdminNavbar';
+import Product from './Pages/Product';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <React.Fragment>
+          <AdminNavbar />
+          <Switch>
+            <Route path='/product' component={Product} />
+          </Switch>
+        </React.Fragment>
+      </BrowserRouter>
     </div>
   );
 }
