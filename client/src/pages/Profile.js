b827fa62d7004cd8e749fee0f71f7a645aba389d
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-28 02:07:11 +0700

    init profile page

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
+
+export default class Profile extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>Profile</h1>
+      </Container>
+    );
+  }
+}
