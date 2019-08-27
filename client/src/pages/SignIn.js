6c308d9bbced036835d894961de3098a67add17d
nguyenhaminhtuan <nguyenhaminhtuan@gmail.com>
2019-08-28 02:05:16 +0700

    fixed logic code, redirect after signin, message has symbols

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -2,18 +2,60 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Container } from 'react-bootstrap';
 import SignInForm from '../components/SignInForm';
+import storage from '../utils/storage';
 
 export default class SingIn extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      user: {}
+      user: {},
+      isSubmit: false,
+      message: ''
     };
+
+    this.signIn = this.signIn.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  signIn(user) {
-    console.log(user);
+  componentDidUpdate() {
+    if (this.state.isSubmit) {
+      this.signIn().then(response => {
+        const { data, status } = response;
+        if (status === 400) {
+          this.setState({
+            message: data.message.split('"').join(''),
+            isSubmit: false
+          });
+        } else if (status === 200) {
+          storage.setToken(data.token);
+          this.props.checkAuth({ isAuth: true });
+          this.props.history.push('/');
+        }
+      });
+    }
+  }
+
+  onSubmit(user) {
+    this.setState({
+      user,
+      isSubmit: true
+    });
+  }
+
+  async signIn() {
+    const { user } = this.state;
+
+    const response = await fetch(`${process.env.REACT_APP_API}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({ email: user.email, password: user.password })
+    });
+    const data = await response.json();
+
+    return { status: response.status, data };
   }
 
   render() {
@@ -34,7 +76,7 @@ export default class SingIn extends Component {
     return (
       <Container className='sign-in mt-5'>
         <FormWrapper>
-          <SignInForm signin={this.signIn} />
+          <SignInForm submit={this.onSubmit} message={this.state.message} />
         </FormWrapper>
       </Container>
     );
